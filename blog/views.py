e47b91979b4b21f3f914580c9e8570b163126a41
Shokhrukh-Barotov <shokhrukh.barotov@gmail.com>
2024-11-08 15:00:41 +0500

    updated wiews.post_list

--- blog/views.py
+++ blog/views.py
@@ -6,33 +6,33 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 
 # Create your views here.
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 5
-    template_name = '../templates/blog/post/list.html'
 
-
-# def post_list(request):
-#     posts_list = Post.published.all()
-#     #Постраничная разбивка с 5 постами на страницу
-#     paginator=Paginator(posts_list, 5)
-#     page_number=request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # Если page_number находится вне диапазона, то
-#         # выдать последнюю страницу
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,
-#                   '../templates/blog/post/list.html',
-#                   {'posts': posts})
+def post_list(request, tag_slug=None):
+    posts_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    #Постраничная разбивка с 5 постами на страницу
+    paginator=Paginator(posts_list, 5)
+    page_number=request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  '../templates/blog/post/list.html',
+                  {'posts': posts,
+                   'tag':tag})
 
 
 def post_detail(request, year, month, day, post):
